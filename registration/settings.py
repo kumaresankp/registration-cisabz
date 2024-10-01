d745e3585ffe180d3a1cda6eb04fd677f257ed6c
Kumaresan <65583580+kumaresankp@users.noreply.github.com>
2024-10-01 08:47:10 +0530

    Update settings.py

--- registration/settings.py
+++ registration/settings.py
@@ -116,8 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "staticfiles/"
-STATIC_ROOT = 'staticfiles'
+STATIC_URL = "/static/"
+STATIC_ROOT = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'staticfiles')
 ]
